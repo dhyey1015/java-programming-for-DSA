d13ce923d7918c619cfc3e9f2584f5d879a41f2f
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-11-13 04:28:16 +0530

    patter-6 right-half-pyramid with numbers

---
+++ lecture-5-patterns/Pattern6.java
@@ -0,0 +1,12 @@
+
+public class Pattern6 {
+
+    public static void main(String args[]){
+        for(int  i = 0; i < 5; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(j+1);
+            }
+            System.out.print("\n");
+        }
+    }
+}
