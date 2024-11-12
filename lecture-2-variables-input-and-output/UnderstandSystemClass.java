5f191c9c6eb8e318fab7ede4b05c1287795e77bb
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-11-12 22:32:50 +0530

    learned about print and println and \n

--- lecture-2-variables-input-and-output/UnderstandSystemClass.java
+++ lecture-2-variables-input-and-output/UnderstandSystemClass.java
@@ -1,9 +1,14 @@
 class UnderstandSystemClass{
     public static void main(String args[]){
         //system is a class in java and out is an instance of some other print related class
-        //which contains print or println
-        // println or print is a function inside the some other print related class
-        System.out.println("Hello, World!!"); 
+        //which contains print or println function
+        //println is used to print things in next line
+        System.out.println("Hello, World!!--------using println"); 
+        //print is used when you don't want your output to be print in next line
+        System.out.print("Hello World----------using print");
+        //"\n" for new line
+        System.out.print("Hello World---------using backslash-n \n");
+        System.out.println("hello");
 
     }
 }
