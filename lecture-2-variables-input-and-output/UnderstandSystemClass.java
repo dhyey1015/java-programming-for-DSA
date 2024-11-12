10ca99dd7c286979d3b185c1fdbc14b6d5586abc
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-11-12 22:22:00 +0530

    learned somethings about System class

---
+++ lecture-2-variables-input-and-output/UnderstandSystemClass.java
@@ -0,0 +1,9 @@
+class UnderstandSystemClass{
+    public static void main(String args[]){
+        //system is a class in java and out is an instance of some other print related class
+        //which contains print or println
+        // println or print is a function inside the some other print related class
+        System.out.println("Hello, World!!"); 
+
+    }
+}
