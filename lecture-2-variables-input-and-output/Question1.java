fefdc1b087f6a3539d137dc0ca7121e0a4d0a2ed
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-11-12 22:44:14 +0530

    completed in class qustion-1

---
+++ lecture-2-variables-input-and-output/Question1.java
@@ -0,0 +1,8 @@
+//Question: 1 
+//print the pattern using print and println or \n
+
+class Question1{
+    public static void main(String args[]){
+        System.out.println("*\n**\n***\n****");
+    }
+}
