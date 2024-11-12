97da2ea8e5906a078715499efee8ce597c69ca64
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-11-12 23:36:21 +0530

    learned about data types and some basic memory allocation

---
+++ lecture-2-variables-input-and-output/Sum.java
@@ -0,0 +1,10 @@
+class Sum{
+    public static void main(String args[]){
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+        int mul = a * b;
+        System.out.println("sum" + sum);
+        System.out.println("multiplicaton" + mul);
+    }
+}
