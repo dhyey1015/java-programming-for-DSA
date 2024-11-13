f6976bb5bf60203ab9d1b389a487e5e24a43f243
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-11-13 23:47:00 +0530

    advance pattern-2 rombus

---
+++ lecture-6-advanced-patterns/Pattern2.java
@@ -0,0 +1,17 @@
+
+
+public class Pattern2 {
+
+    public static void main(String args[]){
+        int n = 5;
+        for (int i = 0; i < n; i++){
+            for (int j = n; j > i; j--){
+                System.out.print("  ");
+            }
+            for(int j = 0; j < n; j++){
+                System.out.print(" *");
+            }
+        System.out.print("\n");
+        }
+    }
+}
